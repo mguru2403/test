d6fb5782ad7d516beb4a40e82aab3fb02a328721
mguru2403 <mguru.2403@gmail.com>
2022-03-29 15:22:45 +0530

    third commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("test");
+		
+		System.out.println("test1");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
